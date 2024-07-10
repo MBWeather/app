4a9acdf476b16dd3a0d7acfc2ef289dc988627a0
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-10 17:28:41 +0200

    environment updates - #2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,35 @@
 export const environment = {
-  production: true
+  production: true,
+  api: {
+    servers: {
+      primary: {
+        url: 'https://api.openweathermap.org/',
+        api: 'data/',
+        headers: {
+          // Any other default headers can be added here
+        }
+      },
+      backup: 'https://yourbackupserver.com'
+    },
+    primaryHeaders: {
+      'Content-Type': 'application/json',
+      // Any other default headers can be added here
+    }
+  },
+  app: {
+    name: 'MBWeather',
+    version: '1.0.0',
+    config: {
+      language: {
+        default: 'en',
+        available: ['si', 'en'],
+      },
+      storage: {
+        keys: {
+          language: 'languages',
+          selectedLanguage: 'selectedLanguage'
+        }
+      }
+    }
+  }
 };
