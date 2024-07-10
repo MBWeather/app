bd61ea36bc348de979b874d911a6c1b80952dd26
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-10 17:54:58 +0200

    executed basic api call - #2

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private apiService: ApiService
+  ) {}
 
+  public ngOnInit(): void {
+    console.log('Hello, World!');
+
+    this.apiService.get('3.0/onecall', {
+      lat: '33.44',
+      lon: '-94.04'
+    })?.subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
