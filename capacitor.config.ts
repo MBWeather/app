f7bdca9a9016e70ae7888d5b64c5beed37f454c5
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-10 16:24:13 +0200

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'si.mbweather.bakiforhire',
+  appName: 'MBWeather',
+  webDir: 'www'
+};
+
+export default config;
