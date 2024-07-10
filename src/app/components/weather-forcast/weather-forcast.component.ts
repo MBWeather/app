e3d434db20e8b05ce928645cdc868217be7e61ff
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-10 20:05:55 +0200

    Added basic chart styles - #3 & #12

--- src/app/components/weather-forcast/weather-forcast.component.ts
+++ src/app/components/weather-forcast/weather-forcast.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
+import { BaseChartDirective, NG_CHARTS_CONFIGURATION } from 'ng2-charts';
 import { ApiService } from 'src/app/services/api/api.service';
 import { WeatherApiResponse } from 'src/app/types/weather';
 
@@ -7,36 +9,88 @@ import { WeatherApiResponse } from 'src/app/types/weather';
   templateUrl: './weather-forcast.component.html',
   styleUrls: ['./weather-forcast.component.scss'],
 })
-export class WeatherForcastComponent  implements OnInit {
-
+export class WeatherForcastComponent implements OnInit {
   protected loading: boolean = true;
+  protected weatherData!: WeatherApiResponse;
+  protected lastUpdated!: Date;
 
-  protected forecast: any = [];
+  public chartData: ChartData<'line'> = {
+    datasets: [
+      {
+        data: [],
+        label: 'Temperature',
+        fill: false,
+        borderColor: '#4bc0c0'
+      }
+    ],
+    labels: []
+  };
 
-  protected lastUpdated: string = '';
+  public chartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top'
+      }
+    }
+  };
 
-  constructor(
-    private apiService: ApiService
-  ) {}
+  constructor(private apiService: ApiService) { }
 
   public ngOnInit(): void {
-    console.log("Fetching weather data...");
-    this.apiService.get<WeatherApiResponse>('3.0/onecall', {
-      lat: '33.44',
-      lon: '-94.04'
-    })?.subscribe({
-      next: (response: WeatherApiResponse) => {
-        console.log("Weather data fetched successfully.");
-        console.log(response);
-        this.forecast = response.daily;
-      },
-      error: (error) => {
-        console.log("An error occurred while fetching weather data.");
-        console.error(error);
-      }
+    this.triggerFetchWD();
+  }
+
+  protected triggerFetchWD(): void {
+    this.loading = true;
+    this.fetchWeatherData().then(() => {
+      this.loading = false;
+      this.lastUpdated = new Date();
+      this.updateChartData();
     });
   }
 
-  protected refreshForecast(): void {
+  private async fetchWeatherData(): Promise<void> {
+    let hasError = false;
+
+    return new Promise((resolve, reject) => {
+      this.apiService.get<WeatherApiResponse>('3.0/onecall', {
+        lat: '46.5547',
+        lon: '15.6459'
+      })?.subscribe({
+        next: (response: WeatherApiResponse) => {
+          console.log("Weather data fetched successfully.");
+          console.log(response);
+          this.weatherData = response;
+        },
+        error: (error) => {
+          console.log("An error occurred while fetching weather data.");
+          console.error(error);
+
+          // Set hasError to true
+          hasError = true;
+        }, complete: () => {
+          // Resolve the promise if it didn't fail
+          if (hasError) {
+            reject();
+          } else {
+            resolve();
+          }
+        }
+      });
+    });
+  }
+
+  private updateChartData(): void {
+    if (!this.weatherData || !this.weatherData.daily) {
+      console.log("No weather data available.");
+      return;
+    }
+
+    if (this.weatherData) {
+      this.chartData.labels = this.weatherData.daily.map(day => new Date(day.dt * 1000).toLocaleDateString());
+      this.chartData.datasets[0].data = this.weatherData.daily.map(day => day.temp.day - 273.15); // Convert Kelvin to Celsius
+    }
   }
 }
