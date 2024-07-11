a1ab02553e6c4207b01ec010390eec2dac22ebf2
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-11 18:40:15 +0200

    Home page & module refactored - #20 & #21

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { WeatherForcastComponent } from 'src/app/components/weather-forcast/weather-forcast.component';
+import { WeatherForcastComponent } from 'src/app/components/weather-forecast/weather-forecast.component';
 import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { TranslateModule } from '@ngx-translate/core';
+import { CurrentForecastComponent } from 'src/app/components/current-forecast/current-forecast.component';
+import { DailyForecastComponent } from 'src/app/components/daily-forecast/daily-forecast.component';
 
 const IMPORTS = [
   CommonModule,
@@ -18,7 +20,12 @@ const IMPORTS = [
   TranslateModule
 ];
 
-const DECLARATIONS = [HomePage, WeatherForcastComponent];
+const DECLARATIONS = [
+  HomePage, 
+  WeatherForcastComponent,
+  CurrentForecastComponent,
+  DailyForecastComponent
+];
 
 const PROVIDERS = [
   provideCharts(withDefaultRegisterables())
