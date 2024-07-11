437e7afdf5e5f893f3d059675b52eb52c70296a2
BaKiForHire <blaze.banjanski@bakiforhire.com>
2024-07-11 18:56:30 +0200

    Updated app structure - #21

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { WeatherForcastComponent } from 'src/app/components/weather-forecast/weather-forecast.component';
 import { BaseChartDirective, provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { TranslateModule } from '@ngx-translate/core';
-import { CurrentForecastComponent } from 'src/app/components/current-forecast/current-forecast.component';
-import { DailyForecastComponent } from 'src/app/components/daily-forecast/daily-forecast.component';
+import { HomePage } from './pages/home.page';
+import { WeatherForcastComponent } from './components/weather-forecast/weather-forecast.component';
+import { CurrentForecastComponent } from './components/current-forecast/current-forecast.component';
+import { DailyForecastComponent } from './components/daily-forecast/daily-forecast.component';
 
 const IMPORTS = [
   CommonModule,
