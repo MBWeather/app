6c54e65b81af36f1412531a7c2dfeb41c4243f17
Blaže Banjanski <51700255+BaKiForHire@users.noreply.github.com>
2024-07-12 12:49:39 +0200

    Revert "Reload data & translate reimplement"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { langRegEx, LANGUAGES } from './@mbweather/constants';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +6,5 @@ import { langRegEx, LANGUAGES } from './@mbweather/constants';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  private readonly browserLang: string = `${this.translate.getBrowserLang()}`;
-
-  constructor(
-    private translate: TranslateService
-  ) {
-        // Available languages
-        translate.addLangs(LANGUAGES.available.map(lang => lang.short));
-        // Set the default language
-        translate.setDefaultLang(LANGUAGES.default);
-        // Set the language based on the browser language
-        translate.use(this.browserLang.match(langRegEx) ? this.browserLang : LANGUAGES.default);
-  }
+  constructor() {}
 }
--- src/app/components/weather-forcast/weather-forcast.component.spec.ts
+++ src/app/components/weather-forcast/weather-forcast.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { DailyForecastComponent } from './daily-forecast.component';
+import { WeatherForcastComponent } from './weather-forcast.component';
 
-describe('DailyForecastComponent', () => {
-  let component: DailyForecastComponent;
-  let fixture: ComponentFixture<DailyForecastComponent>;
+describe('WeatherForcastComponent', () => {
+  let component: WeatherForcastComponent;
+  let fixture: ComponentFixture<WeatherForcastComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ DailyForecastComponent ],
+      declarations: [ WeatherForcastComponent ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(DailyForecastComponent);
+    fixture = TestBed.createComponent(WeatherForcastComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
